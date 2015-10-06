a3d472b7d238e1f2352062910e4101497ee61cb7
orperel <or_perel@hotmail.com>
2015-10-06 12:52:55 +0300

    Gui main 2

--- ChessProg/ChessProg/ChessMainWindow.h
+++ ChessProg/ChessProg/ChessMainWindow.h
@@ -10,4 +10,12 @@ void setActiveWindow(GuiWindow* window);
 /** Creates the main menu window of the chess game. */
 GuiWindow* createMainMenu();
 
+/** The main loop of the gui, maintains a steady framerate and queries events.
+ *  This loop allows the gui to be sensitive to user actions, and allows windows to switch.
+ *	We always query the active window, so switching windows is easily done by calling setActiveWindow.
+ *	We always draw to the screen in a framerate of 60 fps (configurable). This is important for animations
+ *	and button state effects (on mouse over / click / etc animation).
+ */
+int runGuiMainLoop()
+
 #endif
